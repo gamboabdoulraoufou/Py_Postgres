8eb494d7081063b2e82532cb92e1bf17951ec48d
Abdoul <gamboabdoulraouf@gmail.com>
2015-07-05 12:11:35 +0200

    Update Python_API.py

--- Python_API.py
+++ Python_API.py
@@ -34,40 +34,45 @@ conn.commit()
 # Load data
 def importFromCsv(conn, fname, table):
     with open(fname) as inf:
-        cursor = None
+        cur = None
         try:
-            cursor = conn.cursor()
-            cursor.copy_from(inf, table)
-            cursor.commit()
+            cur = conn.cursor()
+            cur.copy_from(inf, table)
+            cur.commit()
         except psycopg2.DatabaseError, e:
-            if cursor:
-                cursor.rollback()
+            if cur:
+                cur.rollback()
             print 'Error %s' % e    
                 sys.exit(1)
         finally:
-            if cursor:
-                cursor.close()
+            if cur:
+                cur.close()
         inf.close()
 
-list_file = [i for i in os.listdir(conf_file['path']) if os.path.isfile(i)]
+list_file = [i for i in os.listdir(conf_file['inpath']) if os.path.isfile(i)]
 for f in list_file:
-    importFromCsv(conn, os.path.join(conf_file['path'], f), conf_file['table'])
+    importFromCsv(conn, os.path.join(conf_file['inpath'], f), conf_file['table'])
     print("%s data copied" % (f))
     
 
 # Query data
-cur = conn.cursor()
-cur.execute("SELECT categorie,
-                    COUNT(DISTINCT household_key) AS Nb_client,
-                    COUNT(DISTINCT transaction_key) AS Nb_trx ,
-                    SUM(spend_amount) AS CA
-                    from COMPANY
-                    where transaction_date BETWEEN %s AND %s 
-                    GROUP BY categorie" % (conf_file[date_debut], conf_file[date_fin]))
-records = cur.fetchall()
-
+try:
+    cur = conn.cursor()
+    cur.execute("SELECT categorie,
+                        COUNT(DISTINCT household_key) AS Nb_client,
+                        COUNT(DISTINCT transaction_key) AS Nb_trx ,
+                        SUM(spend_amount) AS CA
+                        from COMPANY
+                        where transaction_date BETWEEN %s AND %s 
+                        GROUP BY categorie" % (conf_file[date_debut], conf_file[date_fin]))
+    records = cur.fetchall()
+except psycopg2.DatabaseError, e:
+    print 'Error %s' % e
+finally:
+    cur.close()
+    
 # Save query result in CSV file
-with open('result.csv', 'w') as f:
+with open(conf_file['outpath']+'result.csv', 'w') as f:
     writer = csv.writer(f, delimiter=';')
     for row in records:
         writer.writerow(row)
